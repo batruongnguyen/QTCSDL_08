92243d10e271a56d2fa27a59d455519d514a518d
NgBaTruong <85273668+batruongnguyen@users.noreply.github.com>
2021-11-12 19:35:06 +0700

    up

--- QuanLyChoThueXe_Nhom08/QuanLyChoThueXe_Nhom08/Program.cs
+++ QuanLyChoThueXe_Nhom08/QuanLyChoThueXe_Nhom08/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyChoThueXe_Nhom08
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmGiaoDien());
+            Application.Run(new frmDangNhap());
         }
     }
 }
